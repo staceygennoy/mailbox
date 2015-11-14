8404f8921a4d97253bbfeb927add7663d989d2ab
Stacey Gennoy <sgennoy@gmail.com>
2015-11-13 17:23:24 -0800

    Refinement of the message view

--- mailbox/ViewController.swift
+++ mailbox/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var messageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +23,10 @@ class ViewController: UIViewController {
         scrollView.contentSize = imageView.image!.size
     }
 
+    @IBAction func didPanView(sender: AnyObject) {
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
