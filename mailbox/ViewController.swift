2fa8627d346c21ef29d3a87372a1fec4d16c6369
Stacey Gennoy <sgennoy@gmail.com>
2015-11-13 21:38:10 -0800

    Configured RGB colors for slide backgrounds

--- mailbox/ViewController.swift
+++ mailbox/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var messageView: UIImageView!
     
+    @IBOutlet weak var messageWrapperView: UIView!
+    
     var messageOriginalCenter: CGPoint!
     var messageOffset: CGFloat!
     var messageRight: CGPoint!
@@ -32,25 +34,32 @@ class ViewController: UIViewController {
   
     @IBAction func didPanMessageView(sender: UIPanGestureRecognizer) {
         let translation = sender.translationInView(view)
-        
+        let velocity = sender.velocityInView(view)
         
         if sender.state == UIGestureRecognizerState.Began {
             messageOriginalCenter = messageView.center
             
         } else if sender.state == UIGestureRecognizerState.Changed {
-           
+            // Moving the message view along with user's panning
             messageView.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y)
 
-            
+            // detect when message has moved all the way to either left or right
             let messageLeft = messageView.center.x - 160
-            
             if messageLeft < -280 {
-                print("user swiped all the way to the left")
+             //   print("user swiped all the way to the left")
+            }
+            if messageLeft > 280 {
+             //   print("user swiped all the way to the right")
             }
             
-            if messageLeft > 280 {
-                print("user swiped all the way to the right")
+            // change the color based on which direction
+            if velocity.x > 0 {
+                messageWrapperView.backgroundColor = UIColor.redColor()
             }
+            else if velocity.x < 0 {
+                messageWrapperView.backgroundColor = UIColor.init(colorLiteralRed: 127/255, green: 228/255, blue: 134/255, alpha: 1)
+            }
+            
             
         } else if sender.state == UIGestureRecognizerState.Ended {
             UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] , animations: { () -> Void in
