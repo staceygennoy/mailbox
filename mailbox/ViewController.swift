53a73c6c819b216fc1adbaf9447c3b9922612f30
Stacey Gennoy <sgennoy@gmail.com>
2015-11-13 18:08:00 -0800

    Left and right swipes, release back to center

--- mailbox/ViewController.swift
+++ mailbox/ViewController.swift
@@ -16,15 +16,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var messageView: UIImageView!
     
+    var messageOriginalCenter: CGPoint!
+    var messageOffset: CGFloat!
+    var messageRight: CGPoint!
+    var messageLeft: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         scrollView.contentSize = imageView.image!.size
+        
+        messageOffset = 160
+        messageRight = CGPoint(x: messageView.center.x - messageOffset, y: messageView.center.y)
+        messageLeft = CGPoint(x: messageView.center.x + messageOffset, y: messageView.center.y)
+
     }
 
-    @IBAction func didPanView(sender: AnyObject) {
-    
+  
+    @IBAction func didPanMessageView(sender: UIPanGestureRecognizer) {
+        let velocity = sender.velocityInView(view)
+        let translation = sender.translationInView(view)
+        
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            messageOriginalCenter = messageView.center
+            
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            // only move the message if the new center is greater than or equal to 464
+            //if (messageOriginalCenter.y + translation.y >= 464) {
+                messageView.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y)
+            //}
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] , animations: { () -> Void in
+                self.messageView.center = self.messageOriginalCenter
+            }, completion: {
+                (Bool) -> Void in
+            })
+        }
+
     }
     
     override func didReceiveMemoryWarning() {
