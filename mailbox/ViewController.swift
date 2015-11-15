fdf1d8bf697daac3016e235fedfa04b800ff328d
Stacey Gennoy <sgennoy@gmail.com>
2015-11-14 19:46:51 -0800

    Gray background added for the less than 50 pt

--- mailbox/ViewController.swift
+++ mailbox/ViewController.swift
@@ -20,11 +20,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var archiveViewIcon: UIImageView!
     
+    @IBOutlet weak var deleteViewIcon: UIImageView!
+    
     var messageOriginalCenter: CGPoint!
     var messageOffset: CGFloat!
     var messageRight: CGPoint!
     var messageLeft: CGPoint!
     var archiveOriginalCenter: CGPoint!
+    var deleteOriginalCenter: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,23 +45,34 @@ class ViewController: UIViewController {
         if sender.state == UIGestureRecognizerState.Began {
             messageOriginalCenter = messageView.center
             archiveOriginalCenter = archiveViewIcon.center
+            deleteOriginalCenter = deleteViewIcon.center
             
         } else if sender.state == UIGestureRecognizerState.Changed {
             // Moving the message view along with user's panning
             messageView.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y)
             archiveViewIcon.center = CGPoint(x: archiveOriginalCenter.x + translation.x, y: archiveOriginalCenter.y)
+            deleteViewIcon.center = CGPoint(x: deleteOriginalCenter.x + translation.x, y: deleteOriginalCenter.y)
 
             
             // detect where the message is, in order to set the background color
             let messageLeft = messageView.center.x - 160
-            if (messageLeft < 0) {
+            if (messageLeft >= -50 && messageLeft <= 50) {
+                messageWrapperView.backgroundColor = UIColor.grayColor()
+                archiveViewIcon.alpha = 0
+                deleteViewIcon.alpha = 0
+            }
+            if (messageLeft < -50) {
                 messageWrapperView.backgroundColor = UIColor.yellowColor()
             }
-            if (messageLeft > 0 && messageLeft <= 220) {
+            if (messageLeft > 50 && messageLeft <= 220) {
                 messageWrapperView.backgroundColor = UIColor.init(colorLiteralRed: 127/255, green: 228/255, blue: 134/255, alpha: 1)
+                archiveViewIcon.alpha = 1
+                deleteViewIcon.alpha = 0
             }
             if messageLeft > 220 {
                 messageWrapperView.backgroundColor = UIColor.redColor()
+                archiveViewIcon.alpha = 0
+                deleteViewIcon.alpha = 1
             }
 
             
@@ -66,6 +80,7 @@ class ViewController: UIViewController {
             UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] , animations: { () -> Void in
                 self.messageView.center = self.messageOriginalCenter
                 self.archiveViewIcon.center = self.archiveOriginalCenter
+                self.deleteViewIcon.center = self.deleteOriginalCenter
             }, completion: {
                 (Bool) -> Void in
             })
